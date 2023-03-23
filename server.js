c41c7318052f1ae90d5c6a8f20d37497d413ec27
Yumemira <surrenderyumi@mail.ru>
2023-03-23 19:57:56 +0300

    working algorithm finally whole

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const http = require('http')
 const express = require("express")
 const cors = require('cors')
 const tools = require('./playtools/tools')
+const { Server } = require("socket.io")
 
 const hostname = process.env.DEFAULT_HOST
 const app = express()
 const port = process.env.DEFAULT_PORT
 
+console.log(process.env.REACT_FRONT_PATH)
 app.use(express.urlencoded())
 app.use(cors({origin: process.env.REACT_FRONT_PATH}))
 app.use(express.json())
@@ -22,17 +24,67 @@ const io = new Server(server, {
     },
 })
 
-
 app.post('/crowscare-game-start', function(req,res){
-    var gameInstance = {
-        playerId:req.body.pid,
-        log:[],
-        weights:tools.generateWeights()
+    const inst = gameMeta.find(x => x.playerId === req.body.pid)
+    if(!inst){
+        const gameInstance = {
+            playerId:req.body.pid,
+            playerNum:null,
+            globalLog:[],
+            generatedNum:tools.generateRandomNum(),
+            starting:tools.randomMessage(),
+            log:[],
+            weights:tools.generateWeights(),
+            reserve:[],
+            found:0,
+            stage:1
+        }
+        gameMeta.push(gameInstance)
+        res.json({start:gameInstance.starting,num:gameInstance.playerNum})
     }
-
-    gameMeta.push(gameInstance)
+    else
+    {
+        res.json({start:inst.starting, message:inst.log,num:inst.playerNum})
+    }
 })
 
+app.post('/crowscare-set-number',function(req,res){
+    let elem = gameMeta.find(x => x.playerId === req.body.pid)
+    gameMeta[gameMeta.indexOf(elem)].playerNum = req.body.setting
+    res.json({message:'success'})
+})
+
+app.post('/crowscare-game-suggestion', function(req,res){
+    const suggest = req.body.suggest
+    const searched = gameMeta.find(x => x.playerId === req.body.pid)
+    const ans = tools.answerCompare(suggest,searched.generatedNum.join(''))
+    const index = gameMeta.indexOf(searched)
+    gameMeta[index].globalLog.push({nums:suggest, ans:ans})
+    const cans = tools.createAnswer(searched.playerNum, searched.weights, searched.stage, searched.found, searched.reserve, searched.log)
+    
+    gameMeta[index].reserve = cans.reserve
+    gameMeta[index].weights = cans.weights
+    gameMeta[index].stage = cans.stage
+    gameMeta[index].found = cans.found
+    gameMeta[index].log = cans.log
+
+
+    if(ans[0]===4)
+    {
+        res.json({state:"win"})
+    }
+    else if(cans.log.ans[0] === 4)
+    {
+        res.json({state:"lose"})
+    }
+
+    res.json({message:ans, })
+})
+
+app.post('/crowscare-end-game', function(req,res){
+    gameMeta.splice(gameMeta.indexOf(gameMeta.find(x => x.playerId === req.body.pid)), 1)
+    res.json({message:'success'})
+})
 
 
 server.listen(port, hostname, () => {console.log('starting')})
